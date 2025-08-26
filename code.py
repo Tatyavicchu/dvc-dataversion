3cc1269619482c76705b25d0ca6b1cb94bf3f76d
Mohit Bhandari <112754295+Tatyavicchu@users.noreply.github.com>
2025-08-26 23:45:25 +0530

    second version of data saved

--- code.py
+++ code.py
@@ -11,6 +11,10 @@ data = {
 # 2. Convert to DataFrame
 df = pd.DataFrame(data)
 
+# adding a new row
+new_row={'Name':'john','Age':33,'city':'LA'}
+df.loc[len(df.index)]=new_row
+
 # 3. Create 'data' folder if it doesn't exist
 os.makedirs('data', exist_ok=True)
 
